db66f4cdc43a3ae1cf4fd19c2a9d53f53d224cdb
Yousef Osman <yousef.osman.31@gmail.com>
2024-09-11 10:36:22 +0300

    Auth: Add Mappers

---
+++ CleanArchitecture.Infrastructure/Mappers/IdentityResultMappers.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.DTOs.Auth;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchitecture.Infrastructure.Mappers;
+public static class IdentityResultMappers
+{
+    public static IdentityResultDto ToIdentityResultDto(this IdentityResult result)
+    {
+        return new IdentityResultDto
+        {
+            Succeeded = result.Succeeded,
+            Errors = result.Errors?.Select(x => x.ToIdentityResultDto())
+        };
+    }
+    
+    public static IdentityErrorDto ToIdentityResultDto(this IdentityError error)
+    {
+        return new IdentityErrorDto
+        {
+            Code = error.Code,
+            Description = error.Description,
+        };
+    }
+}
---
+++ CleanArchitecture.Infrastructure/Mappers/UserMappers.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitecture.Application.DTOs.Auth;
+using CleanArchitecture.Infrastructure.Data.Models;
+
+namespace CleanArchitecture.Infrastructure.Mappers;
+public static class UserMappers
+{
+    public static UserDto ToUserDto(this AppUser user)
+    {
+        return new UserDto { 
+            Id = user.Id,
+            UserName = user.UserName,
+            Email = user.Email,
+            EmailConfirmed = user.EmailConfirmed,
+            PhoneNumber = user.PhoneNumber,
+            PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+            PasswordHash = user.PasswordHash,
+        };
+    }
+
+    public static AppUser ToAppUser(this UserDto user)
+    {
+        return new AppUser
+        {
+            Id = user.Id,
+            UserName = user.UserName,
+            Email = user.Email,
+            EmailConfirmed = user.EmailConfirmed,
+            PhoneNumber = user.PhoneNumber,
+            PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+            PasswordHash = user.PasswordHash,
+        };
+    }
+}
