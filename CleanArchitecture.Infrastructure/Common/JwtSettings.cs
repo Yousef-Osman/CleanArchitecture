901ee588e9508c634a2b44959821fbe43354013d
Yousef Osman <yousef.osman.31@gmail.com>
2024-09-12 15:24:09 +0300

    Auth: Add Auth user and token services and dependancy injection

---
+++ CleanArchitecture.Infrastructure/Common/JwtSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Infrastructure.Common;
+public class JwtSettings
+{
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public string Key { get; set; } = null!;
+    public int ExpiresInMinutes { get; set; }
+}
--- CleanArchitecture.Infrastructure/DependencyInjection.cs
+++ CleanArchitecture.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,17 @@
-﻿using CleanArchitecture.Application.Interfaces.Persistence;
+﻿using CleanArchitecture.Application.Interfaces.Identity;
+using CleanArchitecture.Application.Interfaces.Persistence;
+using CleanArchitecture.Infrastructure.Common;
 using CleanArchitecture.Infrastructure.Data;
 using CleanArchitecture.Infrastructure.Data.Models;
 using CleanArchitecture.Infrastructure.Repositories;
+using CleanArchitecture.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace CleanArchitecture.Infrastructure;
 public static class DependencyInjection
@@ -18,14 +24,48 @@ public static class DependencyInjection
 
         services.AddIdentityCore<AppUser>()
             .AddEntityFrameworkStores<AppDbContext>()
-            .AddApiEndpoints();
+            .AddDefaultTokenProviders();
+
+        services.AddAuthentication(config);
 
         services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
         services.AddAuthorizationBuilder();
 
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<ITokenService, TokenService>();
         services.AddScoped(typeof(IReadRepository<,>), typeof(GenericRepository<,>));
         services.AddScoped(typeof(IRepository<,>), typeof(GenericRepository<,>));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<JwtSettings>(config.GetSection(nameof(JwtSettings)));
+
+        var key = Encoding.ASCII.GetBytes(config["JwtSettings:Key"]);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidIssuer = config["JwtSettings:Issuer"],
+                ValidAudience = config["JwtSettings:Audience"],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+        });
 
         return services;
     }
